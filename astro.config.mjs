b6c2cad5d412e0819410d0085fed59ec6ba4b2a5
zlxlty <lus2@carleton.edu>
2026-08-23 11:47:50 -0400

    chore: configure React MDX and Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   output: "static",
+  integrations: [mdx(), react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
